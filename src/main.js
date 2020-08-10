0fea78b5c982d47b707b22edf050412e361b4802
Berkmann18 <maxieberkmann@gmail.com>
2020-08-10 15:26:56 +0100

    New: Ionic Vue app [WIP]

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from 'vue';
+import App from './App.vue';
+
+import Ionic from '@ionic/vue';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
+import { IonicVueRouter } from '@ionic/vue';
+
+Vue.use(IonicVueRouter);
 
 Vue.config.productionTip = false;
+Vue.use(Ionic);
+
+const router = new IonicVueRouter({
+  routes: [
+    { path: '/', redirect: '/home' },
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import(/* webpackChunkName: "home" */ '@/views/Home')
+    },
+    {
+      path: '/new-item',
+      name: 'new-item',
+      component: () => import(/* webpackChunkName: "new-item" */ '@/views/NewItem')
+    }
+  ]
+});
 
 new Vue({
+  router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
---
+++ src/views/NewItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar color="primary">
+        <ion-buttons slot="start">
+          <ion-back-button default-href="/"></ion-back-button>
+        </ion-buttons>
+        <ion-title>New Item</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content padding></ion-content>
+  </ion-page>
+</template>
+
+<script>
+export default {
+  name: 'NewItemPage'
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
